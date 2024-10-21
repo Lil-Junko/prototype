4e40bc317b68909b9757f0b9a9ce052a3d2f7cdd
Nolan Meyer <102100700+Lil-Junko@users.noreply.github.com>
2024-10-21 21:14:20 +0000

    yeah

---
+++ main.cpp
@@ -0,0 +1,47 @@
+
+
+int main()
+{
+    /*
+    * Create a new game
+    * 100 by 100 by 3 grid
+    * for grid[][][0]:
+    * place player in a random side on the right
+    * place deep sea knight on the left
+    * add obstacles around the map
+    * make sure player can move around and reach enemy
+    * when deep sea knight is defeated, move to next level
+    * display "stage complete" message
+    * for grid[][][1]:
+    * place player in a random side on the right
+    * place deep sea knight on the left
+    * modify deep sea knight, at half health switch to stage 2 of boss
+    * when deep sea knight is defeated, move to next level
+    * display "stage complete" message
+    * for grid[][][2]:
+    * place player in a random side on the right
+    * place deep sea knight on the left
+    * modify deep sea knight, starts boss at stage 2
+    * after defeating boss, game is won
+    * display "Foe slain" message
+    */
+
+   /*
+   * display:
+   * a 19 by 19 grid with player in the middle
+   * player can move up(w), down(s), left(a), right(d) twice on their turn or attack(e) once
+   * attack brings up a menu of attack options
+   * player uses SP for certain attacks
+   * player can use items(i) to heal or buff
+   * player can place traps(t) to damage enemies
+   */
+
+    /*
+    * deep sea knight:
+    * update enemy position after player moves or attacks
+    * enemy can move up, down, left, right, or attack
+    * enemy can attack player if player is in range
+    * enemy can move towards player if player is out of range
+    */
+    return 0;
+}
